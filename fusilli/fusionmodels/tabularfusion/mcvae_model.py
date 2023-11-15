84e25e8c30e0a088c20e10844bbecb7fa8db2e18
Florence Townend <f.j.townend@live.com>
2023-11-15 10:28:04 +0000

    updated mcvae imports to sys path to see if that'll fix it

--- fusilli/fusionmodels/tabularfusion/mcvae_model.py
+++ fusilli/fusionmodels/tabularfusion/mcvae_model.py
@@ -11,7 +11,12 @@ import contextlib
 import pandas as pd
 import numpy as np
 from fusilli.utils.training_utils import get_checkpoint_filenames_for_subspace_models
+
 import sys
+
+sys.path.append("fusilli/utils/mcvae")
+from mcvae.models import Mcvae
+from mcvae.models.utils import DEVICE
 
 from fusilli.utils import check_model_validity
 
